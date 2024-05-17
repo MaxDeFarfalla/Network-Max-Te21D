7645df8d04dc5311893f658bee1fac7e5a7ae27f
MaxDeFarfalla <Maximilian.defarfalla@elev.ga.ntig.se>
2024-05-17 12:43:05 +0200

    start

---
+++ src/Server.java
@@ -0,0 +1,11 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public void main(String[] args) {
+        ServerSocket server = null;
+        Socket client;
+
+        //Default port number we are going to use
+    }
+}
